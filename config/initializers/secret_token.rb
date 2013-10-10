89551666b8acd1e171254eec26ef430259f059e5
Alberto Perez <alpemaca@gmail.com>
2013-10-10 14:12:04 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twilater::Application.config.secret_key_base = '0f1a5d88ae7f017c32ff9b03bf5f4c13615fc03f96268356c88530d60b5363f41c53db3063fb2248dee5c487d79f58e6d243b5d5290549b29d8ae106fb7312da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twilater::Application.config.session_store :cookie_store, key: '_twilater_session'
